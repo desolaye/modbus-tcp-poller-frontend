da66d7a6037bc75d87166bfcd69c9a7ea059380e
desolaye <ex4to@icloud.com>
2024-10-28 17:13:43 +0700

    chore: test signal r

--- src/pages/main/lib/use-main-page.tsx
+++ src/pages/main/lib/use-main-page.tsx
@@ -18,11 +18,12 @@ export const useMainPage = () => {
     console.log(data);
   });
 
-  connection.invoke("ReceiveData");
+  // connection.invoke("ReceiveData");
   // connection.stream("ReceiveData");
 
   connection.onclose((err) => console.error(err));
   connection.onreconnecting((err) => console.error(err));
+  connection.start();
 
   return {
     values: {
